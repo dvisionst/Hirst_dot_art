298073c5859023c650ae507f2c8155890a0ace00
flowers186 <62859441+dvisionst@users.noreply.github.com>
2021-10-12 23:16:38 +0000

    Added to actual code to generate an art piece akin to Damien Hirst

--- main.py
+++ main.py
@@ -21,3 +21,36 @@
 
 
 
+import turtle
+from turtle import Turtle, Screen
+import random
+
+
+def hirst_art(my_turtle):
+    my_turtle.penup()
+    my_turtle.forward(50)
+    my_turtle.pendown()
+    my_turtle.dot(20, random.choice(color_list))
+
+
+turtle.colormode(255)
+color_list = [(167, 77, 38), (24, 115, 157), (216, 150, 90), (19, 35, 28), (188, 177, 28), (29, 133, 87),
+              (214, 73, 116), (160, 57, 94), (118, 186, 155), (101, 175, 201), (208, 129, 162), (59, 166, 111),
+              (228, 79, 41), (224, 206, 89), (22, 166, 196), (132, 33, 58), (12, 98, 64), (227, 219, 200),
+              (27, 57, 120), (217, 227, 221), (131, 37, 28), (229, 206, 11), (83, 77, 26), (17, 84, 99),
+              (212, 224, 229), (158, 209, 190), (225, 167, 193)]
+
+h_turtle = Turtle()
+my_screen = Screen()
+h_turtle_x = 0
+h_turtle_y = 0
+
+for rows in range(9):
+    h_turtle.setposition(h_turtle_x, h_turtle_y)
+    for dots in range(9):
+        h_turtle.speed("fastest")
+        hirst_art(h_turtle)
+        h_turtle.penup()
+    h_turtle_y += 50
+
+my_screen.exitonclick()
