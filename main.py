5cb92273eb59ea7030f6466640f46aa4ee72a022
flowers186 <62859441+dvisionst@users.noreply.github.com>
2021-10-12 22:18:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is the initial code used to extract a list of tupples
+# from an image file in order to complete the Hirst Painting Project
+
+
+# import colorgram
+# 
+#
+# def value_extract(color_obj):
+#     rgb = color_obj.rgb
+#     red = rgb[0]
+#     green = rgb[1]
+#     blue = rgb[2]
+#     color_tup = (red, green, blue)
+#     return color_tup
+#
+#
+# colors = colorgram.extract('hirst_dot.jpg', 30)
+# rgb_list = []
+# for color_extracted in colors:
+#     rgb_list.append(value_extract(color_extracted))
+
+
+
